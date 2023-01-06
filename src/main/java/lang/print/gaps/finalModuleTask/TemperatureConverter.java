de8d122105e18da380b23db74840ca451b689cbb
Usmon Kurbanbaev <usmonkurbanbaev1@gmail.com>
2023-01-06 19:08:08 +0500

    for final task

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        double toFahrenheit = (9.0 / 5) * temperatureCelsius + 32;
+        System.out.println(toFahrenheit);
     }
 }
